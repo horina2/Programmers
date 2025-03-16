0ddb5baddeebe10179354e6cfa39e1f517d9f257
horina2 <horina923@gmail.com>
2025-03-16 11:55:39 +0900

    전화번호 목록 두번째

--- 레벨2_전화번호_목록/Solution.java
+++ 레벨2_전화번호_목록/Solution.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = false;
+        boolean answer = true;
         
         // 전화번호부 정렬 
         Arrays.sort(phone_book);
         
         for(int i=1; i<phone_book.length; i++) {
         	if(phone_book[i].contains(phone_book[i-1])) {
-        		answer = true;
+        		answer = false;
         		return answer;
         	}
         }
