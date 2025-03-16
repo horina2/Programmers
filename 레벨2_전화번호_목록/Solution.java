f27e0713408b8872a62fbabb12a4789868f397a7
horina2 <horina923@gmail.com>
2025-03-16 11:43:00 +0900

    전화번호 목록 첫번째

--- 레벨2_전화번호_목록/Solution.java
+++ 레벨2_전화번호_목록/Solution.java
@@ -1,5 +1,21 @@
 package 레벨2_전화번호_목록;
 
-public class Solution {
-	
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = false;
+        
+        // 전화번호부 정렬 
+        Arrays.sort(phone_book);
+        
+        for(int i=1; i<phone_book.length; i++) {
+        	if(phone_book[i].contains(phone_book[i-1])) {
+        		answer = true;
+        		return answer;
+        	}
+        }
+        
+        return answer;
+    }
 }
