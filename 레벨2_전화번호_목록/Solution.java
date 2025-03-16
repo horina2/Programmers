9bbba3962542bad7224e4c43590f6c340b8c54c2
horina2 <horina923@gmail.com>
2025-03-16 11:59:52 +0900

    전화번호 목록 세번째

--- 레벨2_전화번호_목록/Solution.java
+++ 레벨2_전화번호_목록/Solution.java
@@ -10,7 +10,7 @@ class Solution {
         Arrays.sort(phone_book);
         
         for(int i=1; i<phone_book.length; i++) {
-        	if(phone_book[i].contains(phone_book[i-1])) {
+        	if(phone_book[i].startsWith(phone_book[i-1])) {
         		answer = false;
         		return answer;
         	}
